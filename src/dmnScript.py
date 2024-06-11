f7a35f42c287aa2c6448366f9f2f57394605cc7d
WilliamBrandt <william.brandt@student.hpi.de>
2024-06-11 09:59:01 +0200

    adapt dmnScript to call ocel2Parser + minor changes to ocelExample.json

--- src/dmnScript.py
+++ src/dmnScript.py
@@ -1,10 +1,11 @@
 from dmnParser import DMNParser
 from dmnEvaluator import DMNEvaluator
 from genericObject import GenericObject
+from ocel2Parser import OCEL
 
 
-orderDMNpath = 'processModel/orderStateDMN.dmn'
-invoiceDMNpath = 'processModel/invoiceStateDMN.dmn'
+orderDMNpath = '../processModel/orderStateDMN.dmn'
+invoiceDMNpath = '../processModel/invoiceStateDMN.dmn'
 
 parser = DMNParser()
 orderDMN = parser.parse(orderDMNpath)
@@ -14,12 +15,19 @@ invoiceDMN = parser.parse(invoiceDMNpath)
 dmnTables = [orderDMN[0], invoiceDMN[0]]
 
 # Creating an instance
-invoiceId = "asda-21231-a21as"
-order = GenericObject(clazz="class.order", id="123", totalamount=150, confirmed=True, history=['Create Invoice', "ArchiveOrder"], invoice=invoiceId)
-invoice = GenericObject (clazz="class.invoice", id = invoiceId, receivedate="2024-05-30", history=["SentInvoice"])
+ocel = OCEL()
+# default loads /processModel/ocelExample.json
+ocel.parse_and_store()
 
-objects = [order, invoice]
-object = order
+# generate needed objects
+objects = ocel.get_objects_with_history_and_foreign_key()
+object = objects[0]
+# old example
+# invoiceId = "asda-21231-a21as"
+# order = GenericObject(clazz="class.order", id="123", totalamount=150, confirmed=True, history=['Create Invoice', "ArchiveOrder"], invoice=invoiceId)
+# invoice = GenericObject (clazz="class.invoice", id = invoiceId, receivedate="2024-05-30", history=["SentInvoice"])
+# objects = [order, invoice]
+# object = order
 
 evaluator = DMNEvaluator(dmnTables, objects, debugging=True)
 validStates = evaluator.evaluate(object)
