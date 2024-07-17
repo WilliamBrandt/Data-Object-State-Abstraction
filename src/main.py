3067644b310a0fb48a304abc21b183049b12850f
Raban Gießler <raban.giessler@student.hpi.de>
2024-07-17 10:34:29 +0200

    chg: main script for presentation

--- src/main.py
+++ src/main.py
@@ -1,34 +1,26 @@
 from dmnParser import DMNParser
 from dmnEvaluator import DMNEvaluator
 from ocel2Parser import OCEL
-import pathlib
 
 # Define paths to data files
-path = str(pathlib.Path().resolve())
 orderDMNpath = '../data/O2C_orderStateDMN.dmn'
 invoiceDMNpath = '../data/O2C_invoiceStateDMN.dmn'
-ocelPath = '../data/O2C_ocel.json'
+ocelPath = '../data/O2C_ocel_final_presentation_v1.json'
 
 # Parse DMN tables
 parser = DMNParser()
 orderDMN = parser.parse(orderDMNpath)
 invoiceDMN = parser.parse(invoiceDMNpath)
-
 dmnTables = [orderDMN[0], invoiceDMN[0]] # '[0]' is necessary because the parser returns a list of DMNTables
 
-
-# Creating an instance
+# Extract objects from OCEL
 ocel = OCEL()
-# default loads /processModel/ocelExample.json
 ocel.parse_and_store(ocelPath)
-
-# generate needed objects
 objects = ocel.get_objects_with_history_and_foreign_key()
-object = objects[0]
 
+# Initialize evaluator and test for cyclic state dependencies
 evaluator = DMNEvaluator(dmnTables, objects, debugging=True)
 
-# evaluator.visualizeGraph()
-validStates = evaluator.evaluate(object)
-
-print(validStates)
+# Evaluate object
+order = objects[0]
+validStates = evaluator.evaluate(order)
